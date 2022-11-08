f224276dd64041b58b894bfc1452571912f9839f
beardfriend <beardfriend21@gmail.com>
2022-11-08 21:25:06 +0900

    비즈니스 확인 로직 에러 수정

--- internal/app/service/academy_service.go
+++ internal/app/service/academy_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"encoding/json"
 	"errors"
 
 	"onthemat/pkg/openapi"
@@ -22,8 +23,17 @@ func NewAcademyService(businessAPI *openapi.BusinessMan) AcademyService {
 
 func (s *academyService) VerifyBusinessMan(businessCode string) error {
 	resp := s.businessAPI.GetStatus(businessCode)
-	if resp.StatusCode() != 200 {
-		return errors.New("인증되지 않은 비즈니스 번호입니다.")
+
+	var result struct {
+		Data []struct {
+			Bstt string `json:"b_stt"`
+		} `json:"data"`
+	}
+
+	json.Unmarshal(resp.Body(), &result)
+
+	if result.Data[0].Bstt == "" {
+		return errors.New("사업자 번호를 다시 확인해주세요")
 	}
 	return nil
 }
