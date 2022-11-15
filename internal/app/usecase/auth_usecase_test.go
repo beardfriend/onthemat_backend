68e96897171b787beec4d79d090cdbec5be9d5d8
beardfriend <beardfriend21@gmail.com>
2022-11-15 12:01:09 +0900

    usecase 테스트 리팩토링

--- internal/app/usecase/auth_usecase_test.go
+++ internal/app/usecase/auth_usecase_test.go
@@ -10,110 +10,105 @@ import (
 	"onthemat/internal/app/config"
 	"onthemat/internal/app/mocks"
 	"onthemat/internal/app/model"
-	"onthemat/internal/app/service/token"
 	"onthemat/internal/app/transport"
 	"onthemat/internal/app/usecase"
-	"onthemat/pkg/auth/jwt"
 	"onthemat/pkg/ent"
 	"onthemat/pkg/kakao"
 	pkgMock "onthemat/pkg/mocks"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestAuthUC_CheckDuplicateEmail(t *testing.T) {
+func TestAuthUC_TestSuite(t *testing.T) {
+	suite.Run(t, new(AuthUC_TestSuite))
+}
+
+type AuthUC_TestSuite struct {
+	suite.Suite
+	authUC usecase.AuthUseCase
+
+	mockTokenService *mocks.TokenService
+	mockUserRepo     *mocks.UserRepository
+	mockAuthService  *mocks.AuthService
+	mockStore        *pkgMock.Store
+}
+
+// 모든 테스트 시작 전 1회
+func (ts *AuthUC_TestSuite) SetupSuite() {
 	c := config.NewConfig()
 
-	mockTokenService := new(mocks.TokenService)
+	ts.mockTokenService = new(mocks.TokenService)
 
-	mockUserRepo := new(mocks.UserRepository)
-	mockAuthService := new(mocks.AuthService)
-	mockStore := new(pkgMock.Store)
+	ts.mockUserRepo = new(mocks.UserRepository)
+	ts.mockAuthService = new(mocks.AuthService)
+	ts.mockStore = new(pkgMock.Store)
+	ts.authUC = usecase.NewAuthUseCase(ts.mockTokenService, ts.mockUserRepo, ts.mockAuthService, ts.mockStore, c)
+}
 
+func (ts *AuthUC_TestSuite) TestAuthUC_CheckDuplicateEmail() {
 	email := "sample@mail.com"
-	t.Run("success", func(t *testing.T) {
-		mockUserRepo.On("FindByEmail", mock.Anything, mock.AnythingOfType("string")).Return(false, nil).Once()
-
-		authUC := usecase.NewAuthUseCase(mockTokenService, mockUserRepo, mockAuthService, mockStore, c)
-		err := authUC.CheckDuplicatedEmail(context.TODO(), email)
-		assert.NoError(t, err)
+	ts.Run("success", func() {
+		ts.mockUserRepo.On("FindByEmail", mock.Anything, mock.AnythingOfType("string")).Return(false, nil).Once()
+		err := ts.authUC.CheckDuplicatedEmail(context.TODO(), email)
+		ts.NoError(err)
 	})
-
-	t.Run("email-already-exisit", func(t *testing.T) {
-		mockUserRepo.On("FindByEmail", mock.Anything, mock.AnythingOfType("string")).Return(true, nil).Once()
-
-		authUC := usecase.NewAuthUseCase(mockTokenService, mockUserRepo, mockAuthService, mockStore, c)
-		err := authUC.CheckDuplicatedEmail(context.TODO(), email)
+	ts.Run("email-already-exisit", func() {
+		ts.mockUserRepo.On("FindByEmail", mock.Anything, mock.AnythingOfType("string")).Return(true, nil).Once()
+		err := ts.authUC.CheckDuplicatedEmail(context.TODO(), email)
 		errorStruct := err.(common.HttpError)
-		assert.Equal(t, 409, errorStruct.ErrCode)
-		assert.Equal(t, "이미 존재하는 이메일입니다.", errorStruct.ErrDetails)
+		ts.Equal(409, errorStruct.ErrCode)
+		ts.Equal("이미 존재하는 이메일입니다.", errorStruct.ErrDetails)
 	})
 }
 
-func TestAuthUC_Login(t *testing.T) {
-	c := config.NewConfig()
-
-	mockTokenService := new(mocks.TokenService)
-	mockUserRepo := new(mocks.UserRepository)
-	mockAuthService := new(mocks.AuthService)
-	mockStore := new(pkgMock.Store)
-
+func (ts *AuthUC_TestSuite) TestAuthUC_Login() {
 	userEmail := "asd@naver.com"
 	userPassword := "password"
 
-	t.Run("유저 정보가 없을 경우", func(t *testing.T) {
-		mockUserRepo.On("GetByEmailPassword", mock.Anything, mock.AnythingOfType("*ent.User")).Return(nil, &ent.NotFoundError{}).Once()
+	ts.Run("유저 정보가 없을 경우", func() {
+		ts.mockAuthService.On("HashPassword", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("hashedPassword")
+		ts.mockUserRepo.On("GetByEmailPassword", mock.Anything, mock.AnythingOfType("*ent.User")).Return(nil, &ent.NotFoundError{}).Once()
 
-		authUC := usecase.NewAuthUseCase(mockTokenService, mockUserRepo, mockAuthService, mockStore, c)
-		_, err := authUC.Login(context.TODO(), &transport.LoginBody{
+		_, err := ts.authUC.Login(context.TODO(), &transport.LoginBody{
 			Email:    "asd@naver.com",
 			Password: "password",
 		})
+
 		errorStruct := err.(common.HttpError)
-		assert.Equal(t, 404, errorStruct.ErrCode)
-		assert.Equal(t, "이메일 혹은 비밀번호를 다시 확인해주세요.", errorStruct.ErrDetails)
+		ts.Equal(404, errorStruct.ErrCode)
+		ts.Equal("이메일 혹은 비밀번호를 다시 확인해주세요.", errorStruct.ErrDetails)
 	})
 
-	t.Run("이메일 인증이 되지 않은 경우", func(t *testing.T) {
-		mockUserRepo.On("GetByEmailPassword", mock.Anything, mock.AnythingOfType("*ent.User")).Return(&ent.User{
+	ts.Run("이메일 인증이 되지 않은 경우", func() {
+		ts.mockAuthService.On("HashPassword", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return("hashedPassword")
+		ts.mockUserRepo.On("GetByEmailPassword", mock.Anything, mock.AnythingOfType("*ent.User")).Return(&ent.User{
 			Email:           &userEmail,
 			Password:        &userPassword,
 			IsEmailVerified: false,
 		}, nil).Once()
 
-		authUC := usecase.NewAuthUseCase(mockTokenService, mockUserRepo, mockAuthService, mockStore, c)
-		_, err := authUC.Login(context.TODO(), &transport.LoginBody{
+		_, err := ts.authUC.Login(context.TODO(), &transport.LoginBody{
 			Email:    userEmail,
 			Password: userPassword,
 		})
 
 		errorStruct := err.(common.HttpError)
-		assert.Equal(t, 400, errorStruct.ErrCode)
-		assert.Equal(t, "이메일 인증이 필요합니다.", errorStruct.ErrDetails)
+		ts.Equal(400, errorStruct.ErrCode)
+		ts.Equal("이메일 인증이 필요합니다.", errorStruct.ErrDetails)
 	})
 }
 
-func TestAuthUC_SocialLogin(t *testing.T) {
-	c := config.NewConfig()
-
-	mockTokenService := new(mocks.TokenService)
-	mockUserRepo := new(mocks.UserRepository)
-	mockAuthService := new(mocks.AuthService)
-	mockStore := new(pkgMock.Store)
-	jwt := jwt.NewJwt().WithSignKey("signKey").Init()
-
-	tokenSvc := token.NewToken(jwt)
-
+func (ts *AuthUC_TestSuite) TestAuthUC_SocialLogin() {
 	redirectCode := "examplecode"
 	sociaKey := "123123123"
 	email := "asd@naver.com"
 	nickname := "nickname"
 
-	t.Run("성공", func(t *testing.T) {
-		t.Run("카카오 로그인 (최초 접근)", func(t *testing.T) {
+	ts.Run("성공", func() {
+		ts.Run("카카오 로그인 (최초 접근)", func() {
 			keyInt, _ := strconv.Atoi(sociaKey)
-			mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
+			ts.mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
 				Return(&kakao.GetUserInfoSuccessBody{
 					Id: uint(keyInt),
 					KakaoAccount: struct {
@@ -132,11 +127,11 @@ func TestAuthUC_SocialLogin(t *testing.T) {
 				}, nil).
 				Once()
 
-			mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
+			ts.mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
 				Return(nil, nil).
 				Once()
 
-			mockUserRepo.On("Create", mock.Anything, mock.AnythingOfType("*ent.User")).
+			ts.mockUserRepo.On("Create", mock.Anything, mock.AnythingOfType("*ent.User")).
 				Return(&ent.User{
 					ID:          1,
 					TermAgreeAt: time.Now(),
@@ -147,78 +142,74 @@ func TestAuthUC_SocialLogin(t *testing.T) {
 				}, nil).
 				Once()
 
-			mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
 				Return("refreshToken", nil).
 				Once()
 
-			mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
+			ts.mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
 
-			mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
 				Return("AccessToken", nil).
 				Once()
 
-			mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
 				Return(time.Now()).
 				Once()
 
-			mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
 				Return(time.Now()).Once()
 
 			// 검증
-			authUC := usecase.NewAuthUseCase(mockTokenService, mockUserRepo, mockAuthService, mockStore, c)
-			l, err := authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
+			l, err := ts.authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
 
-			assert.Equal(t, l.AccessToken, "AccessToken")
-			assert.NoError(t, err, nil)
+			ts.Equal(l.AccessToken, "AccessToken")
+			ts.NoError(err, nil)
 		})
 
-		t.Run("카카오 로그인 이미 가입한 유저", func(t *testing.T) {
+		ts.Run("카카오 로그인 이미 가입한 유저", func() {
 			keyInt, _ := strconv.Atoi(sociaKey)
-			mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
+			ts.mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
 				Return(&kakao.GetUserInfoSuccessBody{
 					Id: uint(keyInt),
 				}, nil).
 				Once()
 
-			mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
+			ts.mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
 				Return(&ent.User{ID: 1, SocialKey: &sociaKey, SocialName: &model.KakaoSocialType}, nil).
 				Once()
 
-			mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
 				Return("refreshToken", nil).
 				Once()
 
-			mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
+			ts.mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
 
-			mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
 				Return("AccessToken", nil).
 				Once()
 
-			mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
 				Return(time.Now()).
 				Once()
 
-			mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
 				Return(time.Now()).Once()
-			authUC := usecase.NewAuthUseCase(tokenSvc, mockUserRepo, mockAuthService, mockStore, c)
-			l, err := authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
 
-			claim := &token.TokenClaim{}
-			tokenSvc.ParseToken(l.AccessToken, claim)
-			assert.Equal(t, claim.LoginType, "kakao")
-			assert.Equal(t, claim.UserType, "")
-			assert.NoError(t, err, nil)
+			l, err := ts.authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
+
+			ts.Equal(l.AccessToken, "AccessToken")
+			ts.NoError(err, nil)
 		})
 
-		t.Run("카카오 로그인 학원선생님 인증을 마친 유저", func(t *testing.T) {
+		ts.Run("카카오 로그인 학원선생님 인증을 마친 유저", func() {
 			keyInt, _ := strconv.Atoi(sociaKey)
-			mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
+			ts.mockAuthService.On("GetKakaoInfo", mock.AnythingOfType("string")).
 				Return(&kakao.GetUserInfoSuccessBody{
 					Id: uint(keyInt),
 				}, nil).
 				Once()
 
-			mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
+			ts.mockUserRepo.On("GetBySocialKey", mock.Anything, mock.Anything).
 				Return(&ent.User{
 					ID:         1,
 					SocialKey:  &sociaKey,
@@ -229,20 +220,27 @@ func TestAuthUC_SocialLogin(t *testing.T) {
 				}, nil).
 				Once()
 
-			mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
 				Return("refreshToken", nil).
 				Once()
 
-			mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
+			ts.mockStore.On("Set", mock.Anything, mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("time.Duration")).Return(nil).Once()
 
-			authUC := usecase.NewAuthUseCase(tokenSvc, mockUserRepo, mockAuthService, mockStore, c)
-			l, err := authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
+			ts.mockTokenService.On("GenerateToken", mock.AnythingOfType("string"), mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("int")).
+				Return("AccessToken", nil).
+				Once()
 
-			claim := &token.TokenClaim{}
-			tokenSvc.ParseToken(l.AccessToken, claim)
-			assert.Equal(t, claim.LoginType, "kakao")
-			assert.Equal(t, claim.UserType, "academy")
-			assert.NoError(t, err, nil)
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+				Return(time.Now()).
+				Once()
+
+			ts.mockTokenService.On("GetExpiredAt", mock.AnythingOfType("int")).
+				Return(time.Now()).Once()
+
+			l, err := ts.authUC.SocialLogin(context.TODO(), model.KakaoSocialType, redirectCode)
+
+			ts.Equal(l.AccessToken, "AccessToken")
+			ts.NoError(err, nil)
 		})
 	})
 }
