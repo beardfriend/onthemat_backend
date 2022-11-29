e11253df9c6ddadb6f7fc3f965945bb4841a891d
beardfriend <beardfriend21@gmail.com>
2022-11-29 09:42:20 +0900

    요가 문서작업 및 코드정리

--- internal/app/delivery/http/yoga_handler.go
+++ internal/app/delivery/http/yoga_handler.go
@@ -51,20 +51,21 @@ func NewYogaHandler(
 	g.Post("/group", middleware.Auth, middleware.OnlySuperAdmin, handler.CreateGroup)
 	// 요가 그룹 수정
 	g.Put("/group/:id", middleware.Auth, middleware.OnlySuperAdmin, handler.UpdateGroup)
-
+	// 요가 그룹 부분 수정
+	g.Patch("/group/:id", middleware.Auth, middleware.OnlySuperAdmin, handler.PatchGroup)
 	// 요가 그룹 멀티삭제
-	g.Delete("/groups", middleware.Auth, middleware.OnlySuperAdmin, handler.DeleteGroups)
+	g.Delete("/group/:ids", middleware.Auth, middleware.OnlySuperAdmin, handler.DeleteGroup)
 	// 요가 그룹 리스트
-	g.Get("/group/list", middleware.Auth, handler.GetGroups)
+	g.Get("/groups", middleware.Auth, handler.GetGroups)
 }
 
-// 요가 등록
+// 요가 생성
 /**
-@api {post} /yoga 요가 등록
+@api {post} /yoga 요가 생성
 @apiName postYoga
 @apiVersion 1.0.0
 @apiGroup yoga
-@apiDescription 요가 등록 (어드민 권한만)
+@apiDescription 요가 생성 (어드민 권한만)
 @apiHeader Authorization accessToken (Bearer)
 @apiBody {Number} yogaGroupId 요가 그룹의 아이디
 @apiBody {String} nameKor 요가 이름 한국어
@@ -237,6 +238,68 @@ func (h *yogaHandler) Delete(c *fiber.Ctx) error {
 	})
 }
 
+// 요가 리스트 조회
+/**
+@api {delet} /yoga/list 요가 리스트 조회
+@apiName listYoga
+@apiVersion 1.0.0
+@apiGroup yoga
+@apiDescription 요가 리스트 조회
+
+@apiQuery {Number} groupId 요가 그룹 아이디
+@apiSuccess {Number} code 200
+@apiSuccess {String} message ""
+@apiSuccess {Object[]} result
+@apiSuccess {Number} result.id 아이디
+@apiSuccess {String} result.nameKor 요가 이름 (한국어)
+@apiSuccess {String} [result.nameEng] 요가 이름 (영어)
+@apiSuccess {Number="1,2,3,4,5"} [result.level] 요가 난이도
+@apiSuccess {String} [result.description] 요가에 대한 설명
+@apiSuccess {String} result.createdAt 생성일시
+@apiSuccess {String} result.updatedAt 업데이트일시
+@apiError QueryMissing <code>400</code> code: 3001
+@apiError InternalServerError <code>500</code> code: 500
+*/
+func (h *yogaHandler) ListByGroupId(c *fiber.Ctx) error {
+	ctx := c.Context()
+	reqQuery := new(request.YogaListQuery)
+
+	if err := c.QueryParser(reqQuery); err != nil {
+		return c.Status(http.StatusBadRequest).
+			JSON(ex.NewHttpError(ex.ErrQueryStringMissing, nil))
+	}
+	data, err := h.yogaUsecase.List(ctx, reqQuery.GroupId)
+	if err != nil {
+		return utils.NewError(c, err)
+	}
+
+	resp := response.NewYogaListResponse(data)
+
+	return c.Status(http.StatusOK).JSON(ex.ResponseWithData{
+		Code:    http.StatusOK,
+		Message: "",
+		Result:  resp,
+	})
+}
+
+// 요가 그룹 등록
+/**
+@api {post} /yoga/group 요가 그룹 생성
+@apiName postYogaGroup
+@apiVersion 1.0.0
+@apiGroup yogaGroup
+@apiDescription 요가 생성 (어드민 권한만)
+@apiHeader Authorization accessToken (Bearer)
+@apiBody {String} category 요가 카테고리 (한국어)
+@apiBody {String} categoryEng 요가 카테고리 (영어)
+@apiBody {String} [description] 요가에 대한 설명
+@apiSuccess (201) {Number} code 201
+@apiSuccess (201) {String} message ""
+@apiError JsonMissing <code>400</code> code: 3000
+@apiError ValidateErrors <code>400</code> code: 2000~
+@apiError ErrYogaGroupAlreadyExist <code>409</code> code: 4008
+@apiError InternalServerError <code>500</code> code: 500
+*/
 func (h *yogaHandler) CreateGroup(c *fiber.Ctx) error {
 	ctx := c.Context()
 
@@ -261,22 +324,41 @@ func (h *yogaHandler) CreateGroup(c *fiber.Ctx) error {
 	})
 }
 
+// 요가 그룹 수정
+/**
+@api {put} /yoga/group/:id 요가 그룹 수정
+@apiName putYogaGroup
+@apiVersion 1.0.0
+@apiGroup yogaGroup
+@apiDescription 요가 그룹 수정 (어드민 권한만)
+@apiHeader Authorization accessToken (Bearer)
+@apiParam {Number} id 요가 그룹 아이디
+@apiBody {String} category 요가 카테고리 (한국어)
+@apiBody {String} categoryEng 요가 카테고리 (영어)
+@apiBody {String} [description] 요가에 대한 설명
+@apiSuccess (201) {Number} code 201
+@apiSuccess (201) {String} message ""
+@apiError JsonMissing <code>400</code> code: 3000
+@apiError ParamsMissing <code>400</code> code: 3002
+@apiError ValidateErrors <code>400</code> code: 2000~
+@apiError InternalServerError <code>500</code> code: 500
+*/
 func (h *yogaHandler) UpdateGroup(c *fiber.Ctx) error {
 	ctx := c.Context()
 
 	reqBody := new(request.YogaGroupUpdateBody)
-	reqParam := new(request.YogaUpdateParam)
 	if err := c.BodyParser(reqBody); err != nil {
 		return c.Status(http.StatusBadRequest).
 			JSON(ex.NewHttpError(ex.ErrJsonMissing, nil))
 	}
+	if err := h.validator.ValidateStruct(reqBody); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(ex.NewInvalidInputError(err))
+	}
+
+	reqParam := new(request.YogaUpdateParam)
 	if err := c.ParamsParser(reqParam); err != nil {
 		return c.Status(http.StatusBadRequest).
 			JSON(ex.NewHttpError(ex.ErrParamsMissing, err.Error()))
-	}
-
-	if err := h.validator.ValidateStruct(reqBody); err != nil {
-		return c.Status(http.StatusBadRequest).JSON(ex.NewInvalidInputError(err))
 	}
 
 	if err := h.yogaUsecase.UpdateGroup(ctx, reqBody, reqParam.Id); err != nil {
@@ -289,20 +371,77 @@ func (h *yogaHandler) UpdateGroup(c *fiber.Ctx) error {
 	})
 }
 
-func (h *yogaHandler) DeleteGroups(c *fiber.Ctx) error {
+// 요가 그룹 부분 수정
+/**
+@api {patch} /yoga/group/:id 요가 그룹 부분 수정
+@apiName patchYogaGroup
+@apiVersion 1.0.0
+@apiGroup yogaGroup
+@apiDescription 요가 그룹 부분 수정 (어드민 권한만)
+@apiHeader Authorization accessToken (Bearer)
+@apiParam {Number} id 요가 그룹 아이디
+@apiBody {String} [category] 요가 카테고리 (한국어)
+@apiBody {String} [categoryEng] 요가 카테고리 (영어)
+@apiBody {String} [description] 요가에 대한 설명
+@apiSuccess (201) {Number} code 201
+@apiSuccess (201) {String} message ""
+@apiError JsonMissing <code>400</code> code: 3000
+@apiError ParamsMissing <code>400</code> code: 3002
+@apiError ValidateErrors <code>400</code> code: 2000~
+@apiError InternalServerError <code>500</code> code: 500
+*/
+func (h *yogaHandler) PatchGroup(c *fiber.Ctx) error {
 	ctx := c.Context()
 
-	reqBody := new(request.YogaGroupsDeleteBody)
+	reqBody := new(request.YogaGroupPatchBody)
 	if err := c.BodyParser(reqBody); err != nil {
 		return c.Status(http.StatusBadRequest).
 			JSON(ex.NewHttpError(ex.ErrJsonMissing, nil))
 	}
-
 	if err := h.validator.ValidateStruct(reqBody); err != nil {
 		return c.Status(http.StatusBadRequest).JSON(ex.NewInvalidInputError(err))
 	}
 
-	_, err := h.yogaUsecase.DeleteGroup(ctx, reqBody.Ids)
+	reqParam := new(request.YogaGroupPatchParam)
+	if err := c.ParamsParser(reqParam); err != nil {
+		return c.Status(http.StatusBadRequest).
+			JSON(ex.NewHttpError(ex.ErrParamsMissing, err.Error()))
+	}
+
+	if err := h.yogaUsecase.PatchGroup(ctx, reqBody, reqParam.Id); err != nil {
+		return utils.NewError(c, err)
+	}
+
+	return c.Status(http.StatusCreated).JSON(ex.Response{
+		Code:    http.StatusCreated,
+		Message: "",
+	})
+}
+
+// 요가 그룹 삭제
+/**
+@api {delete} /yoga/group/:ids 요가 그룹 삭제
+@apiName deleteYogaGroup
+@apiVersion 1.0.0
+@apiGroup yogaGroup
+@apiDescription 요가 그룹 삭제 (어드민 권한만)
+@apiHeader Authorization accessToken (Bearer)
+@apiParam {Number} ids 요가 그룹 아이디 ,로 멀티삭제
+@apiSuccess (200) {Number} code 200
+@apiSuccess (200) {String} message ""
+@apiError ParamsMissing <code>400</code> code: 3002
+@apiError InternalServerError <code>500</code> code: 500
+*/
+func (h *yogaHandler) DeleteGroup(c *fiber.Ctx) error {
+	ctx := c.Context()
+
+	reqParam := new(request.YogaGroupDeleteParam)
+	if err := c.ParamsParser(reqParam); err != nil {
+		return c.Status(http.StatusBadRequest).
+			JSON(ex.NewHttpError(ex.ErrJsonMissing, nil))
+	}
+
+	_, err := h.yogaUsecase.DeleteGroup(ctx, reqParam.Ids)
 	if err != nil {
 		return utils.NewError(c, err)
 	}
@@ -313,6 +452,29 @@ func (h *yogaHandler) DeleteGroups(c *fiber.Ctx) error {
 	})
 }
 
+// 요가 그룹 리스트 조회
+/**
+@api {delet} /yoga/groups 요가 그룹 리스트 조회
+@apiName listYogaGroup
+@apiVersion 1.0.0
+@apiGroup yogaGroup
+@apiDescription 요가 그룹 리스트 조회
+@apiQuery {Number} [pageNo=1] 페이지 번호
+@apiQuery {Number} [pageSize=10] 페이지 당 문서 개수
+@apiQuery {String} [category] 검색할 카테고리 이름
+@apiQuery {String="DESC,ASC"} [orderType=DESC] 정렬 타입
+@apiSuccess {Number} code 200
+@apiSuccess {String} message ""
+@apiSuccess {Object[]} result
+@apiSuccess {Number} result.id 아이디
+@apiSuccess {String} result.category 요가 그룹 이름 (한국어)
+@apiSuccess {String} result.categoryEng 요가 그룹 이름 (영어)
+@apiSuccess {String} [result.description] 요가 그룹에 대한 설명
+@apiSuccess {String} result.createdAt 생성일시
+@apiSuccess {String} result.updatedAt 업데이트일시
+@apiError QueryMissing <code>400</code> code: 3001
+@apiError InternalServerError <code>500</code> code: 500
+*/
 func (h *yogaHandler) GetGroups(c *fiber.Ctx) error {
 	ctx := c.Context()
 
@@ -337,27 +499,5 @@ func (h *yogaHandler) GetGroups(c *fiber.Ctx) error {
 		Message:    "",
 		Result:     resp,
 		Pagination: paginationInfo,
-	})
-}
-
-func (h *yogaHandler) ListByGroupId(c *fiber.Ctx) error {
-	ctx := c.Context()
-	reqQuery := new(request.YogaListQuery)
-
-	if err := c.QueryParser(reqQuery); err != nil {
-		return c.Status(http.StatusBadRequest).
-			JSON(ex.NewHttpError(ex.ErrQueryStringMissing, nil))
-	}
-	data, err := h.yogaUsecase.List(ctx, reqQuery.GroupId)
-	if err != nil {
-		return utils.NewError(c, err)
-	}
-
-	resp := response.NewYogaListResponse(data)
-
-	return c.Status(http.StatusOK).JSON(ex.ResponseWithData{
-		Code:    http.StatusOK,
-		Message: "",
-		Result:  resp,
 	})
 }
--- internal/app/transport/request/yoga_request.go
+++ internal/app/transport/request/yoga_request.go
@@ -4,25 +4,9 @@ import (
 	"onthemat/internal/app/utils"
 )
 
-// ------------------- Body -------------------
-type YogaGroupCreateBody struct {
-	Category    string  `json:"category" validate:"required"`
-	CategoryEng string  `json:"categoryEng"`
-	Description *string `json:"description"`
-}
+// ------------------- Yoga -------------------
 
-type YogaGroupUpdateBody struct {
-	Category    string  `json:"category" validate:"required"`
-	CategoryEng string  `json:"categoryEng"`
-	Description *string `json:"description"`
-}
-
-type YogaGroupPatchBody struct {
-	Category    *string `json:"category"`
-	CategoryEng *string `json:"categoryEng"`
-	Description *string `json:"description"`
-}
-
+// ___________ Create ___________
 type YogaCreateBody struct {
 	YogaGroupId int     `json:"yogaGroupId" validate:"required"`
 	NameKor     string  `json:"nameKor" validate:"required"`
@@ -31,6 +15,7 @@ type YogaCreateBody struct {
 	Description *string `json:"description"`
 }
 
+// ___________ Update ___________
 type YogaUpdateBody struct {
 	YogaGroupId int     `json:"yogaGroupId" validate:"required"`
 	NameKor     string  `json:"nameKor"`
@@ -39,6 +24,11 @@ type YogaUpdateBody struct {
 	Description *string `json:"description"`
 }
 
+type YogaUpdateParam struct {
+	Id int `params:"id"`
+}
+
+// ___________ Patch ___________
 type YogaPatchBody struct {
 	YogaGroupId *int    `json:"yogaGroupId,omitempty" validate:"required"`
 	NameKor     *string `json:"nameKor,omitempty"`
@@ -47,15 +37,57 @@ type YogaPatchBody struct {
 	Description *string `json:"description,omitempty"`
 }
 
-type YogaGroupsDeleteBody struct {
-	Ids []int `json:"ids" validate:"required"`
+type YogaPatchParam struct {
+	Id int `params:"id"`
 }
 
-type YogaRawCreateBody struct {
-	YogaName string `json:"name" validate:"required"`
+// ___________ Delete ___________
+
+type YogaDeleteParam struct {
+	Id int `params:"id"`
 }
 
-// ------------------- Query String -------------------
+// ___________ List ___________
+
+type YogaListQuery struct {
+	GroupId int `query:"groupId"`
+}
+
+// ------------------- YogaGroup -------------------
+
+// ___________ Create ___________
+type YogaGroupCreateBody struct {
+	Category    string  `json:"category" validate:"required"`
+	CategoryEng string  `json:"categoryEng"`
+	Description *string `json:"description"`
+}
+
+// ___________ Update ___________
+
+type YogaGroupUpdateBody struct {
+	Category    string  `json:"category" validate:"required"`
+	CategoryEng string  `json:"categoryEng"`
+	Description *string `json:"description"`
+}
+
+// ___________ Patch ___________
+type YogaGroupPatchBody struct {
+	Category    *string `json:"category"`
+	CategoryEng *string `json:"categoryEng"`
+	Description *string `json:"description"`
+}
+
+type YogaGroupPatchParam struct {
+	Id int `params:"id"`
+}
+
+// ___________ Delete ___________
+
+type YogaGroupDeleteParam struct {
+	Ids []int `params:"ids"`
+}
+
+// ___________ List ___________
 
 type YogaGroupListQueries struct {
 	PageNo    int     `query:"pageNo"`
@@ -73,26 +105,13 @@ func NewYogaGroupListQueries() *YogaGroupListQueries {
 	}
 }
 
-type YogaListQuery struct {
-	GroupId int `query:"groupId"`
-}
+// ------------------- Yoga Raw -------------------
 
+type YogaRawCreateBody struct {
+	YogaName string `json:"name" validate:"required"`
+}
 type YogaRawListQuery struct {
 	UserId int `query:"userId"`
-}
-
-// ------------------- Params -------------------
-
-type YogaUpdateParam struct {
-	Id int `params:"id"`
-}
-
-type YogaPatchParam struct {
-	Id int `params:"id"`
-}
-
-type YogaDeleteParam struct {
-	Id int `params:"id"`
 }
 
 type YogaDeleteRawParam struct {
